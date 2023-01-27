9cd7fa1f72df89038ac343f7b1200eb4ae0813eb
Tommy Tran <haitran1995@gmail.com>
2023-01-27 00:33:14 -0500

    add comments

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+// Define type of variable
+const firstName: string = 'Tommy'
+const age: number = 27;
+
+// Define argument type in functions
 const increment = (counterVal: number) => {
   return counterVal + 1;
 }
 
-const counter = increment(10);
+const counter: number = increment(10);
