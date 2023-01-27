b73d2fc53caac22948113549c2e20c9cd05ca4a5
Tommy Tran <haitran1995@gmail.com>
2023-01-27 00:37:23 -0500

    show array string exp

--- build/index.js
+++ build/index.js
@@ -1,6 +1,11 @@
 "use strict";
+// Define type of variable
 const firstName = 'Tommy';
 const age = 27;
+// Define type of array
+let programmingLanguages = ['javascript', 'ruby', 'python'];
+programmingLanguages.push('golang');
+// Define argument type in functions
 const increment = (counterVal) => {
     return counterVal + 1;
 };
--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@
 const firstName: string = 'Tommy'
 const age: number = 27;
 
+// Define type of array
+let programmingLanguages: string[] = ['javascript', 'ruby', 'python']
+programmingLanguages.push('golang');
+
 // Define argument type in functions
 const increment = (counterVal: number) => {
   return counterVal + 1;
